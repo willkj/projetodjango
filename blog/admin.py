fec0f272f82d677eb8ae6ad6b199412c586b27b0
William F. <48420172+willkj@users.noreply.github.com>
2025-07-13 18:13:26 -0300

    Commit cap3
    commit capt 3

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from blog.models import BlogPost
 # Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+    list_filter = (
+        'created_at',
+        'update_at',
+    )
+
+    search_fields = (
+        'title',
+    )
+
+admin.site.register(BlogPost, BlogPostAdmin)
